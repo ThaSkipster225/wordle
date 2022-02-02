8b6548f0260e813c684334407f0b264fa206d907
gabriellesumner <gabbysumner1010@gmail.com>
2022-02-02 17:29:47 -0500

    Added logic and questions for Skippy

--- HAL.py
+++ HAL.py
@@ -46,13 +46,73 @@ def makeguess(wordlist, guesses=[], feedback=[]):
     
     # Use random.choice(bestwords) here if we have not made a single guess yet.
     # This list of words is the best set of starting words to use.
+
+
+    # QUESTIONS FOR SKIPPY:
+
+    # Do we want to have variables for the feedback? Like the number of green/yellow we have figured out
+    # We would have to loop through each element of feedback and only count it once 
+    # (like the same green letter in guesses 1 and 2 should only count as 1 green)
+    num_of_green = 0
+    num_of_yellow = 0
+    yellow_letters = []
+    character_counter = 0
+
+    # This is helpful for totals, but it doesnt take into account similar/repeated yellow letters OR repeated green letters
+    # OR letters turning from yellow to green
+    for list in feedback:
+        character_counter = -1
+        for element in list:
+            character_counter += 1
+            if element == 1:
+                if (guesses[-1][character_counter]) in yellow_letters:
+                    continue
+                else:
+                    num_of_yellow +=1
+                    yellow_letters.append(guesses[-1][character_counter])
+            elif element == 2:
+                num_of_green += 1
+    
+    # We would get rid of the print statements, these are just to see if the iteration was working
+    print(f"the number of green is {num_of_green}")
+    print(f"the number of yellow is {num_of_yellow}")
+    print(f"the letters in the word are {yellow_letters}")
+
+    # I'm having issues with the above code changing between runs...
+    # Idk if we can even use the code, but it might be helpful
+    
+
+    # OR should we combine the feedback from each guess somehow?
+
+    # Idea for our best words/secondary words...
+    # What if our first set of words was all anagrams like "stare", "tears", etc.
+    # Our second set would then be the next common words
+    # Would we have to create a third set, fourth set? Like all anagrams, but slowly using less common letters?
+
+
+    # I commented out the logic below to test it with random words
+    return random.choice(wordlist)
+
+    # FIRST WORD
+    # If it is the first guess, pick a word from bestwords
     if len(guesses) == 0:
         print (wordlist[1])
         print (wordlist[wordlist.index(random.choice(bestwords))])
         return wordlist[wordlist.index(random.choice(bestwords))] 
         # random.choice( list(set(wordlist) - (set(wordlist)-set(bestwords))))
-    elif len(guesses) > 0:
+
+
+    # BASED ON FEEDBACK
+    # else if it is guesses 5-6 and there is some sort of feedback or there is enough feedback...
+    # enough feedback : at least 2 green letters or 1 green and at least 2 yellow or at least 3 yellow
+    elif (len(guesses) >= 5 and feedback != 0) or (num_of_green) >= 2 or (num_of_green == 1 and num_of_yellow >= 2) or (num_of_yellow >= 3):
+        # return based on the feedback
         return random.choice(wordlist)
+
+
+    # "LETTER FARMING"/GOOD FOLLOWUP WORD
+    # else pick a "good" word
+    # good word : commonly used letters, no letter overlap with previous guesses
     else:
         return random.choice(wordlist)
 
