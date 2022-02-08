3bcff14dce25a7129ef443d693bd81e6e5f6c026
gabriellesumner <gabbysumner1010@gmail.com>
2022-02-08 13:47:14 -0500

    Made progress on flag strategy

--- HAL.py
+++ HAL.py
@@ -48,12 +48,8 @@ def makeguess(wordlist, guesses=[], feedback=[]):
     # This list of words is the best set of starting words to use.
 
 
-    # QUESTIONS FOR SKIPPY:
-
-    # Do we want to have variables for the feedback? Like the number of green/yellow we have figured out
-    # We would have to loop through each element of feedback and only count it once 
-    # (like the same green letter in guesses 1 and 2 should only count as 1 green)
-
+    
+    # Create lists and variables to keep track of feedback
     yellow_letters = []     # Keeps track of the letters that have been yellow
     character_counter = 0   # Counts the character we are on - Iterator
     green_letters = ['','','','','']    # Keeps track of the location of the green letters
@@ -62,17 +58,20 @@ def makeguess(wordlist, guesses=[], feedback=[]):
 
     list_counter = 0 # Counts the list we are on - Iterator
 
-    # This is helpful for totals, but it doesnt take into account similar/repeated yellow letters OR repeated green letters
-    # OR letters turning from yellow to green
+    # go through each list in feedback
     for list in feedback:
         
         character_counter = -1
+
+        # go through each character in the list
         for element in list:
             character_counter += 1
             
+            # if it is a 2, add the letter to the green letter list
             if element == 2:
                 green_letters[character_counter] = guesses[list_counter][character_counter] # adds green letters to the right spot in green_letters list
 
+            # if it is a 1, add the letter to the yellow letter list UNLESS it is in green or already in yellow
             elif element == 1:
                 if (guesses[list_counter][character_counter]) in green_letters:
                     continue # Skips over letter if the letter is in list already
@@ -81,6 +80,7 @@ def makeguess(wordlist, guesses=[], feedback=[]):
                 else:
                     yellow_letters.append(guesses[list_counter][character_counter])  # Adds yellow letters to yellow_letters list
             
+            # if it is a 0, add the letter to the grey letter list UNLESS it is already in green OR is in green or yellow
             elif element == 0:
                 if (guesses[list_counter][character_counter]) in grey_letters:
                     continue # Skips over letter if the letter is in list already
@@ -107,45 +107,33 @@ def makeguess(wordlist, guesses=[], feedback=[]):
     print(f"the grey letters in the word are {grey_letters}")
     print(f"Green Letters: {green_letters}")
 
-    # I'm having issues with the above code changing between runs...
-    # Idk if we can even use the code, but it might be helpful
-    
+   
 
-    # OR should we combine the feedback from each guess somehow?
-
-    # Idea for our best words/secondary words...
-    # What if our first set of words was all anagrams like "stare", "tears", etc.
-    # Our second set would then be the next common words
-    # Would we have to create a third set, fourth set? Like all anagrams, but slowly using less common letters?
+    # USE FILTERS TO DETERMINE THE NEXT WORD GUESS
 
     # Finding the starting value for the filter
-    if (len(guesses) <= 1):
-        
+    
+    if len(guesses) == 0: # if it is the first guess, return a random word
+        return random.choice(wordlist)
+    elif (len(guesses) == 1): # if it is the second guess, start the list from the beginning
         start_index = 0
-    else:
-        
+    else: # otherwise, start the list from where we last guessed      
         start_index = wordlist.index(guesses[-1])
     
-
-    # Filter through the wordlist
     
-    green_empty = False
-
     # Check if green_letters list is empty
+    green_empty = True # assume the list is empty
     for i in range (0, len(green_letters)): # go through each index of green_letters
         if green_letters[i] != '': # if the index is not empty
             green_empty = False # empty condtion is false
             break # break out of the loop
-        else:
-            if i == 4: # else, if all indexes have been checked, the green_letters is empty
-                green_empty = True
 
     # Loop for whole of wordlist
     for i in range (start_index, len(wordlist)): # go through each word in the list, i represents the index of that word in the list
         
-        # Create flags that are checked for each word in the list, they all start as falseish
+        # Create flags that are checked for each word in the list, they all start as true
         green_flag = True
-        yellow_flag = False
+        yellow_flag = True
         grey_flag = False
         
         # if the green_letters list is empty, the word automtically passes the green letter check
@@ -158,12 +146,36 @@ def makeguess(wordlist, guesses=[], feedback=[]):
                     continue
                 else: # else, check the word in wordlist with the green letter
                     if wordlist[i][x] != green_letters[x]: # if the wordlist word at that index does not equal the green_letter at that index
-                        # Move to yellow checker
-                        green_flag = False
+                        green_flag = False # set the green_flag to false and break out of the green letter check loop
                         break
         
+        # if the green letter check does not pass, continue to the next word in the list
+        if green_flag == False:
+            continue
+
+        # Check that all yellow letters are in the word
+        for y in range (0, len(yellow_letters)): # go through each yellow letter in the list
+            if yellow_letters[y] not in wordlist[i]: # if the letter is not in the word
+                yellow_flag == False # the word does not pass the yellow letter check
+
+        # if the yellow letter check does not pass, continue to the next word in the list
+        if yellow_flag == False:
+            continue
+
+        # Check that all grey letters are not in the word
+        for z in range (0, len(grey_letters)): # go through each grey letter in the list
+            if grey_letters[z] in wordlist[i]: # if the letter is in the word
+                grey_flag == False # the word does not pass the grey letter check
+
+        # if the grey letter check does not pass, continue to the next word in the list
+        if grey_flag == False:
+            continue
+
+        # if all checks have been passed, return the word
+        if green_flag == True and yellow_flag == True and grey_flag == True:
+            return wordlist[i]
     
-    print(f"the length of wordlist is {len(wordlist)}")
+    # print(f"the length of wordlist is {len(wordlist)}")
 
     # # Check for yellow letters
     # for i in range(0, len(wordlist)-1): # go through each word in the list, i represents the index of that word in the list
@@ -177,12 +189,12 @@ def makeguess(wordlist, guesses=[], feedback=[]):
 
     # FIRST WORD
     # If it is the first guess, pick a random word
-    if len(guesses) == 0:
-        return random.choice(wordlist) 
+    # if len(guesses) == 0:
+    #     return random.choice(wordlist) 
 
-    elif len(guesses) >= 1:
-        # utilize feedback
-        return random.choice(wordlist)
+    # elif len(guesses) >= 1:
+    #     # utilize feedback
+    #     return random.choice(wordlist)
         
         
 
@@ -198,8 +210,8 @@ def makeguess(wordlist, guesses=[], feedback=[]):
     # "LETTER FARMING"/GOOD FOLLOWUP WORD
     # else pick a "good" word
     # good word : commonly used letters, no letter overlap with previous guesses
-    else:
-        return random.choice(wordlist)
+    # else:
+    #     return random.choice(wordlist)
 
     # Use feedback to determine the location of the letters within the word, if it's 2, it stays there.
     # If it's a 1, then it is in the word, but needs to be moved elsewhere in the word. Make the AI guess where it would be.
